06e7c1083a4b20c552671c7056a07a0f9b7fb6e2
murphytarra <murphytarra@gmail.com>
2023-01-25 10:34:45 +0000

    updated subplots

--- tara_analysis.py
+++ tara_analysis.py
@@ -50,40 +50,46 @@ class Nanopore():
 
     def plot_one_event(self, event, peaks=False, print_heights=False): #Works
         file = self.event_set[event]
-        fig, ax = plt.subplots()
-        fig.subplots_adjust(bottom=0.2)
+        '''fig, axs = plt.subplots(2,1, figsize=(8, 6))
+        fig.subplots_adjust(bottom=0.2)'''
         if peaks==False:
+            fig, axs = plt.subplots(1,1, figsize=(8, 6))
+            fig.subplots_adjust(bottom=0.2)
             properties = peaks_dict=self.peaks_one_event(event, self.h,self.t,self.d,self.p,self.w,self.wl )
             samplefreq_Hz = file.attrs["samplefreq_Hz"]
             current = file['current_nA'][...]
             x = np.arange(current.size) / samplefreq_Hz * 1000
             plt.style.use('default')
-            ax.plot(x, current, color='black',linewidth=0.5)
+            axs.plot(x, current, color='black',linewidth=0.5)
             plt.title("Event: " + event)
             plt.xlabel('Time [ms]')
             plt.ylabel('Current Drop[nA]')
-            if print_heights==True:
-                axbox = fig.add_axes([0.1, -0.05, 0.9, 0.05])
-                text_box = TextBox(axbox, "Number of Peaks", textalignment="left")
-                text_box.set_val(len(properties['peaks']) ) # Trigger `submit` with the initial string.
+            show=plt.show()
         elif peaks==True: 
+            fig, axs = plt.subplots(2,1, figsize=(8, 6))
+            fig.subplots_adjust(bottom=0.2)
             properties = peaks_dict=self.peaks_one_event(event, self.h,self.t,self.d,self.p,self.w,self.wl )
             samplefreq_Hz = file.attrs["samplefreq_Hz"]
             current = file['current_nA'][...]
             y = np.arange(current.size) / samplefreq_Hz * 1000
             x= -1*current
             plt.style.use('default')
-            ax.plot(y, -x, color='black',linewidth=0.5)
-            ax.plot(peaks_dict['peaks']/ samplefreq_Hz * 1000, -x[peaks_dict['peaks'] ], 'x', color='red')
-            plt.title("Event: " + event)
-            plt.xlabel('Time [ms]')
-            plt.ylabel('Current Drop[nA]')
-            if print_heights==True:
-                axbox = fig.add_axes([0.1, -0.05, 0.9, 0.05])
-                text_box = TextBox(axbox, "Number of Peaks", textalignment="left")
-                text_box.set_val(len(properties['peaks']) ) # Trigger `submit` with the initial string.
+            axs[0].plot(y, -x, color='black',linewidth=0.5)
+            axs[0].plot(peaks_dict['peaks']/ samplefreq_Hz * 1000, -x[peaks_dict['peaks'] ], 'x', color='red')
+            fig.suptitle("Event: " + event)
+            axs[0].set_in_layout(False)
+            fig.tight_layout()
+            axs[0].set(xlabel='Time [ms]', ylabel='Current Drop[nA]')
+            axs[1].get_xaxis().set_visible(False)
+            axs[1].get_yaxis().set_visible(False)
+            str1 = 'Number of Peaks: ' + str(len(properties['peaks']) )
+            str2 = 'Peak Heights: ' + str((properties['peak_heights']) )
+            axs[1].text(0.05, 0.95, str2, transform=axs[1].transAxes, fontsize=10, 
+            verticalalignment='top')
+            axs[1].text(0.05, 0.55, str1, transform=axs[1].transAxes, fontsize=10, 
+            verticalalignment='top')
             
-        show= plt.show()
+            show= plt.show()
         return show
 
     def plot_events(self, ln = False): #works'
@@ -100,4 +106,3 @@ class Nanopore():
         show= plt.show()
         return show
 
-##########################################################################################
